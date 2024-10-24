46c0d0dcdcaf331e79ee3082ce6d5973ec9eb2e6
Luis Melendez <luis.zdn@gmail.com>
2024-10-24 10:05:30 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Reto 5',
+  webDir: 'www'
+};
+
+export default config;
